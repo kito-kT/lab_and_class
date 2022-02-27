76e92e4bc67cb78dd09046f0a307482c78cf61da
kito <63220087+kito-kT@users.noreply.github.com>
2022-02-26 18:54:31 -0800

    Add files via upload

--- traces.py
+++ traces.py
@@ -2,23 +2,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib as mlt
+from termcolor import colored
+from pyfiglet import Figlet
 
-print("Welcome to traces! \n Please ensure your file is in this folder\n ")
-up1 = input('Please enter file name: ')
+f = Figlet(font='jazmine')
+print(colored(f.renderText('traces'), 'green'))
 
-up2 = input('Is your file from the hPLC? (Y/n) \n') #because hPLC exports csv files with utf-16
 
-if up2 =='Y' or 'y':
+print("\n \nWelcome to traces! \n \n***Please ensure your file is in this folder***\n ")
 
-    df = pd.read_csv(up1, delimiter='\t',encoding='utf-16', header=None)
 
-    df.columns = ['Time (min)', 'GFP Signal (AU)']
+up1 = input('Is your file from the hPLC? (Y/n) \n') #because hPLC exports csv files with utf-16
 
-    user_title = input('What would you like for a title? \n')
+up2 = input('Please enter file name: ')
 
-    # up3 = input('Would you like to subtract baseline value? (Y/n)')
+if up1 =='Y' or up1 == 'y':
 
-    palette = sns.color_palette('rocket_r')
+    df = pd.read_csv(up2, delimiter='\t',encoding='utf-16', header=None)
+
+    up3 = input('\nPlease provide a label for the x-axis \n \n')
+
+    up4 = input('\nPlease provide a label for the y-axis \n \n')
+
+    df.columns = [up3, up4]
+    
+    user_title = input('\n  \nWhat would you like for a title? \n \n')
+
+    print('\nBefore plotting let\'s decide on a color. \nHere are a few available colors: \n magenta = #ff00ff\n chrmison = #DC143C \n blue = #0000FF \n \n')
+
+    up5 = input('Please provide a color using a hex RGB or RGBA string \n \n')
+
+    print('Generating plot now...')
 
     font = {
         'color': 'black',
@@ -26,15 +40,42 @@ if up2 =='Y' or 'y':
         'horizontalalignment': 'center'}
 
     sns.set_context("talk")
-    sns.lineplot(data=df, x='Time (min)', y='GFP Signal (AU)', color='g',palette=palette)
+    sns.lineplot(data=df, x=up3, y=up4, color=up5)
     plt.title(user_title, fontdict=font
         )
     sns.set_theme(style='ticks')
     plt.show()
-    plt.tight_layout()
+    
 
+else:
 
+    print('processing csv as a utf-8 encoded file...')
 
-else :
+    df = pd.read_csv(up2, delimiter='\t',encoding='utf-16', header=None)
 
-    print('Assuming UTF-8... under construction')
+    up3 = input('\nPlease provide a label for the x-axis \n \n')
+
+    up4 = input('\nPlease provide a label for the y-axis \n \n')
+
+    df.columns = [up3, up4]
+    
+    user_title = input('\n  \nWhat would you like for a title? \n \n')
+
+    print('\nBefore plotting let\'s decide on a color. \nHere are a few available colors: \n magenta = #ff00ff\n chrmison = #DC143C \n blue = #0000FF \n \n')
+
+    up5 = input('Please provide a color using a hex RGB or RGBA string \n \n')
+
+    print('Generating plot now...')
+
+    font = {
+        'color': 'black',
+        'verticalalignment': 'baseline',
+        'horizontalalignment': 'center'}
+
+    sns.set_context("talk")
+    sns.lineplot(data=df, x=up3, y=up4, color=up5)
+    plt.title(user_title, fontdict=font
+        )
+    sns.set_theme(style='ticks')
+    plt.show()
+    
